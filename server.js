8c4f397f834ae267842ac887978927e1c821af42
marianovbavaro <marianovbavaro@gmail.com>
2026-02-19 08:46:18 +0100

    Initialize package.json for pv-pdf-generator project

---
+++ server.js
@@ -0,0 +1,16 @@
+{
+  "name": "pv-pdf-generator",
+  "version": "1.0.0",
+  "main": "server.js",
+  "type": "commonjs",
+  "scripts": {
+    "start": "node server.js"
+  },
+  "dependencies": {
+    "dotenv": "^16.4.5",
+    "express": "^4.19.2",
+    "nodemailer": "^6.9.14",
+    "pdf-lib": "^1.17.1",
+    "pg": "^8.12.0"
+  }
+}
