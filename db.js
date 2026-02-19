cc8c645c612aafed2d3b2bdec80468156137cfa6
marianovbavaro <marianovbavaro@gmail.com>
2026-02-19 13:11:10 +0100

    Refactor db.js by removing initDb function
    Removed initDb function and adjusted module exports.

--- db.js
+++ db.js
@@ -1,31 +1,10 @@
 const { Pool } = require("pg");
 
-let cs = process.env.DATABASE_URL;
-
 const pool = new Pool({
-  connectionString: cs,
-  ssl: {
-    rejectUnauthorized: false
-  }
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false },
 });
 
-async function initDb() {
-  await pool.query(`
-    CREATE TABLE IF NOT EXISTS pdfs (
-      id SERIAL PRIMARY KEY,
-      nome TEXT,
-      cognome TEXT,
-      indirizzo TEXT,
-      comune TEXT,
-      codice_fiscale TEXT,
-      pod TEXT,
-      potenza TEXT,
-      pdf_path TEXT,
-      txt_path TEXT,
-      created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-    )
-  `);
-}
+module.exports = { pool };
 
-module.exports = { pool, initDb };
 
